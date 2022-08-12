5bd12b3fc9fd4691138b916322aef3b9d85d150f
plajboi <lyskov.2000@mai.ru>
2022-08-13 00:16:43 +0300

    add responsive to telephonization section

--- components/TelephonizationIndexPage/TelephonizationIndexPage.jsx
+++ components/TelephonizationIndexPage/TelephonizationIndexPage.jsx
@@ -9,7 +9,7 @@ const TelephonizationIndexPage = () => {
     <ContainerLayout>
       <div className="p-6 sm:p-16 md:p-20 rounded-2xl bg-sweetCorn">
 
-        <p className='block text-2xl sm:text-4xl font-bold mb-10 md:mb-12'>
+        <p className='block text-xl sm:text-4xl font-bold mb-10 md:mb-12'>
           Донтехсвязь выполняет работы по телефонизации жилых домов 
           по заявке Товарищества собственников жилья или Управляющей компании.
         </p>
@@ -23,7 +23,7 @@ const TelephonizationIndexPage = () => {
           <span className='font-medium text-xl my-4 sm:my-0 sm:mx-7'>или</span>
 
           <a href="tel:+7(863)202-00-00" className='font-bold sm:text-md md:text-xl 
-            lg:text-2xl flex items-center sm:flex-row'>
+            lg:text-2xl flex items-center sm:flex-row '>
 
             <BsTelephone className='mr-2' />
             +7 (863) 202-00-00
